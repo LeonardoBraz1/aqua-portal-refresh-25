7c72a059be94b5e4bfcab946cd16afc69d5fb754
Leonardo Jesus Braz <101673432+LeonardoBraz1@users.noreply.github.com>
2025-03-18 17:04:15 +0000

    ajustes

--- src/components/sections/ProductsSection.tsx
+++ src/components/sections/ProductsSection.tsx
@@ -15,7 +15,7 @@ const ProductsSection: React.FC<ProductsSectionProps> = ({ products }) => {
         
         <div className="flex flex-col items-center">
           <img 
-            src="/public/aguas.png" 
+            src="./public/aguas.png" 
             alt="Produtos de Água Mineral1" 
             className="w-full h-auto object-contain max-w-4xl mb-10 hover:scale-105 transition-transform duration-500"
           />
